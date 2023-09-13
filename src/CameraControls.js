bc3562b760be3eb1d42f3008f7649aa85a5c936f
bruno31830 <boissiebruno@gmail.com>
2023-09-13 10:49:58 +0200

    Création d'un camera control adapté.

--- src/CameraControls.js
+++ src/CameraControls.js
@@ -1,34 +1,137 @@
-import { useThree } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { useThree, useFrame } from '@react-three/fiber';
 import { useState, useEffect } from 'react';
 import * as THREE from 'three';
+import { Html } from '@react-three/drei';
+import { SkyContext } from './contexts/Skycontext';
+import { useContext } from 'react';
 
 function CameraControls() {
-  const { camera, gl } = useThree();
-  const [lookAt, setLookAt] = useState(new THREE.Vector3(0, 0, -1));
+  const [previousMousePosition, setPreviousMousePosition] = useState({ x: 0, y: 0 });
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const { isLoaded } = useContext(SkyContext);
+  const { camera, gl, size } = useThree();
 
   useEffect(() => {
     function handleMouseMove(event) {
-      // Mettez à jour le point lookAt en fonction du mouvement de la souris
-      setLookAt(new THREE.Vector3(event.clientX, event.clientY, 0));
+      if (!isMouseDown) return;
+
+      const sensitivity = 0.005;
+
+      const deltaX = -(event.offsetX - previousMousePosition.x) * sensitivity;
+      const deltaY = -(event.offsetY - previousMousePosition.y) * sensitivity;
+
+      const horizontalRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), deltaX);
+      const verticalRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), deltaY);
+
+      camera.quaternion.multiply(horizontalRotation);
+      camera.quaternion.multiply(verticalRotation);
+
+      setPreviousMousePosition({ x: event.offsetX, y: event.offsetY });
     }
 
-    // Ajoutez l'eventListener au canvas
     const canvas = gl.domElement;
     canvas.addEventListener('mousemove', handleMouseMove);
 
-    // Assurez-vous de nettoyer l'eventListener lorsque le composant est démonté
     return () => {
       canvas.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [gl.domElement, camera, previousMousePosition, isMouseDown]);
+
+  useEffect(() => {
+    function handleWheel(event) {
+      const zoomStep = 0.25;
+      const zoomChange = Math.sign(event.deltaY) * zoomStep;
+      camera.zoom += zoomChange;
+      camera.zoom = THREE.MathUtils.clamp(camera.zoom, 1, 10);
+      camera.updateProjectionMatrix();
+    }
+
+    const canvas = gl.domElement;
+    canvas.addEventListener('wheel', handleWheel);
+
+    return () => {
+      canvas.removeEventListener('wheel', handleWheel);
+    };
+  }, [gl.domElement, camera]);
+
+
+  
+  useEffect(() => {
+    function handleKeyDown(event) {
+      const rotationAngle = 0.05;  // Ajustez cette valeur pour des rotations plus rapides ou plus lentes
+
+      switch (event.code) {
+        case 'ArrowUp':
+          camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotationAngle);
+          break;
+        case 'ArrowDown':
+          camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), -rotationAngle);
+          break;
+        case 'ArrowLeft':
+          camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotationAngle);
+          break;
+        case 'ArrowRight':
+          camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotationAngle);
+          break;
+        default:
+          break;
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [camera]);
+
+  useEffect(() => {
+    function handleMouseDown(event) {
+      if (event.button === 0) {
+        setIsMouseDown(true);
+        setPreviousMousePosition({ x: event.offsetX, y: event.offsetY });
+      }
+    }
+
+    function handleMouseUp() {
+      setIsMouseDown(false);
+    }
+
+    const canvas = gl.domElement;
+    canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener('mousedown', handleMouseDown);
+      canvas.removeEventListener('mouseup', handleMouseUp);
     };
   }, [gl.domElement]);
 
   useEffect(() => {
-    camera.lookAt(lookAt);
-  }, [lookAt, camera]);
+    camera.position.set(0, 0, 0);
+    camera.updateProjectionMatrix();
+  }, []);
 
-  return <OrbitControls enableZoom={true} />;
+  return (
+    <>
+      <Html>
+        <div style={{
+          position: 'absolute',
+          top: -200,
+          left: -200,
+          color: 'white',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '20px',
+          width: '600px'
+        }}>
+          <span>Zoom: {camera.zoom.toFixed(2)}</span>
+          <span>Position: {camera.position.x.toFixed(2)}, {camera.position.y.toFixed(2)}, {camera.position.z.toFixed(2)}</span>
+          <span>LookAt: {camera.getWorldDirection(new THREE.Vector3()).x.toFixed(2)}, {camera.getWorldDirection(new THREE.Vector3()).y.toFixed(2)}, {camera.getWorldDirection(new THREE.Vector3()).z.toFixed(2)}</span>
+        </div>
+      </Html>
+    </>
+  );
 }
 
 export default CameraControls;
-
--- src/contexts/Skycontext.js
+++ src/contexts/Skycontext.js
@@ -6,15 +6,16 @@ const SkyContext = React.createContext();
 
 function SkyProvider({ children }) {
     const [representation, setRepresentation] = useState("Equatorial");
-    const [shownConstellations,setShownConstellations] = useState("Oui");
+    const [shownConstellations, setShownConstellations] = useState("Oui");
     const [currentTime, setCurrentTime] = useState(new Date());
     const [location, setLocation] = useState({ latitude: null, longitude: null });
     const [starsData, setStarsData] = useState(null);
     const [hipToIndex, setHipToIndex] = useState({});
-
     const [maxShownMagnitude, setMaxShownMagnitude] = useState(6);
     const [constellationLines, setConstellationLines] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
     const R = 1000; // Rayon de la sphère céleste
+
     const toggleRepresentation = () => {
         if (representation === 'Equatorial') {
             setRepresentation('Horizontal');
@@ -22,11 +23,15 @@ function SkyProvider({ children }) {
             setRepresentation('Equatorial');
         }
     };
-    const toggleShownConstellations=()=>{
-        console.log("Toggle shown Constellations")
-        if (shownConstellations==="Oui") setShownConstellations("Non")
-        else setShownConstellations("Oui")
-    }
+
+    const toggleShownConstellations = () => {
+        if (shownConstellations === "Oui") {
+            setShownConstellations("Non");
+        } else {
+            setShownConstellations("Oui");
+        }
+    };
+
     useEffect(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
@@ -45,11 +50,10 @@ function SkyProvider({ children }) {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentTime(new Date());
-        }, 1000);  // Met à jour l'heure chaque seconde
+        }, 1000);
 
-        return () => clearInterval(interval);  // Nettoie l'intervalle lors du démontage du composant
+        return () => clearInterval(interval);
     }, []);
-
     const parseDMS = (dms) => {
         const parts = dms.split(/\s+/);
         return parts.map(part => parseFloat(part));
@@ -59,127 +63,116 @@ function SkyProvider({ children }) {
         const parts = hms.split(/\s+/);
         return parts.map(part => parseFloat(part));
     };
-
     useEffect(() => {
-        fetch(`${process.env.PUBLIC_URL}/datas/hip.tsv`)
+        Promise.all([
+            fetch(`${process.env.PUBLIC_URL}/datas/hip.tsv`).then(response => response.text()),
+            fetch(`${process.env.PUBLIC_URL}/datas/constellation_line_hip.csv`).then(response => response.text())
+        ]).then(([starsDataText, constellationLinesText]) => {
+            // Logique pour traiter starsDataText et constellationLinesText
+            // Traitement de starsDataText
+            let minRA = Infinity;
+            let maxRA = -Infinity;
+            let minDEC = Infinity;
+            let maxDEC = -Infinity;
 
-            .then(response => response.text())
-            .then(data => {
-                let minRA = Infinity;
-                let maxRA = -Infinity;
-                let minDEC = Infinity;
-                let maxDEC = -Infinity;
+            const lines = starsDataText.split('\n').filter(line => !line.startsWith('#') && line.trim() !== '');
 
-                const lines = data.split('\n').filter(line => !line.startsWith('#') && line.trim() !== '');
-            
-                const coords = [];
-                const newMagnitudes = [];
-
-                const RA_INDEX = 1;
-                const DEC_INDEX = 2;
-                const MAG_INDEX = 3;
-
-                // Créez une liste de tous les numéros HIP présents dans le fichier
-                const presentHips = lines.map(line => parseInt(line.split('|')[0])).sort((a, b) => a - b);
-
-                // Créez une liste complète de 1 à 118322
-                const allHips = Array.from({ length: 118322 }, (_, i) => i + 1);
-
-                // Trouvez les numéros HIP manquants
-                const missingHips = allHips.filter(hip => !presentHips.includes(hip));
-
-                console.log("Missing HIP numbers:", missingHips);
-
-                lines.forEach((line, index) => {
-                    const parts = line.split('|');
-                    const hipNumber = parseInt(parts[0], 10);
-                    hipToIndex[hipNumber] = index;
-                    const raParts = parseHMS(parts[RA_INDEX]);
-                    const decParts = parseDMS(parts[DEC_INDEX]);
-
-                    const ra = hmsToRad(...raParts);
-                    const dec = dmsToRad(...decParts);
-                    const mag = parseFloat(parts[MAG_INDEX]);
-
-                    if (ra < minRA) minRA = ra;
-                    if (ra > maxRA) maxRA = ra;
-                    if (dec < minDEC) minDEC = dec;
-                    if (dec > maxDEC) maxDEC = dec;
-
-                    if (!isNaN(mag)) {
-                        newMagnitudes.push(mag);
-                    } else {
-                        console.error("Invalid magnitude value:", parts[MAG_INDEX]);
-                    }
-
-                    const x = R * Math.cos(dec) * Math.cos(ra);
-                    const z = R * Math.cos(dec) * Math.sin(ra);
-                    const y = R * Math.sin(dec);
-
-                    if (!isNaN(x) && !isNaN(y) && !isNaN(z)) {
-                        coords.push(x, y, z);
-                    } else {
-                        console.error("Problem with HIP=" + parts[0]);
-                    }
-
-                    // Vérifier la taille des tableaux de données
-                    if ((coords.length / 3) !== newMagnitudes.length) {
-                        console.error("Mismatch detected at line index:", index);
-                        console.error("Line content:", line);
-                        throw new Error("Mismatch in data arrays"); // This will stop further processing
-                    }
-                });
+            const coords = [];
+            const newMagnitudes = [];
+            const RA_INDEX = 1;
+            const DEC_INDEX = 2;
+            const MAG_INDEX = 3;
 
 
-                console.log("Nb étoiles:" + newMagnitudes.length);
-                console.log("RA Range:", minRA, "-", maxRA);
-                console.log("DEC Range:", minDEC, "-", maxDEC);
+            // Créez une liste de tous les numéros HIP présents dans le fichier
+            const presentHips = lines.map(line => parseInt(line.split('|')[0])).sort((a, b) => a - b);
 
-                // Vérifier la gamme de magnitudes
-                const validMagnitudes = newMagnitudes.filter(mag => !isNaN(mag));
+            // Créez une liste complète de 1 à 118322
+            const allHips = Array.from({ length: 118322 }, (_, i) => i + 1);
 
-                const minMagnitude = Math.min(...validMagnitudes);
-                const maxMagnitude = Math.max(...validMagnitudes);
-                console.log("Magnitude Range:", minMagnitude, "-", maxMagnitude);
+            // Trouvez les numéros HIP manquants
+            const missingHips = allHips.filter(hip => !presentHips.includes(hip));
 
-                // Afficher un échantillon des étoiles
-                // console.log("Sample stars:", lines.slice(0, 10));
+            console.log("Missing HIP numbers:", missingHips);
+
+            lines.forEach((line, index) => {
+                const parts = line.split('|');
+                const hipNumber = parseInt(parts[0], 10);
+                hipToIndex[hipNumber] = index;
+                const raParts = parseHMS(parts[RA_INDEX]);
+                const decParts = parseDMS(parts[DEC_INDEX]);
+
+                const ra = hmsToRad(...raParts);
+                const dec = dmsToRad(...decParts);
+                const mag = parseFloat(parts[MAG_INDEX]);
+
+                if (ra < minRA) minRA = ra;
+                if (ra > maxRA) maxRA = ra;
+                if (dec < minDEC) minDEC = dec;
+                if (dec > maxDEC) maxDEC = dec;
+
+                if (!isNaN(mag)) {
+                    newMagnitudes.push(mag);
+                } else {
+                    console.error("Invalid magnitude value:", parts[MAG_INDEX]);
+                }
+
+                const x = R * Math.cos(dec) * Math.cos(ra);
+                const z = R * Math.cos(dec) * Math.sin(ra);
+                const y = R * Math.sin(dec);
+
+                if (!isNaN(x) && !isNaN(y) && !isNaN(z)) {
+                    coords.push(x, y, z);
+                } else {
+                    console.error("Problem with HIP=" + parts[0]);
+                }
+
+                // Vérifier la taille des tableaux de données
+                if ((coords.length / 3) !== newMagnitudes.length) {
+                    console.error("Mismatch detected at line index:", index);
+                    console.error("Line content:", line);
+                    throw new Error("Mismatch in data arrays"); // This will stop further processing
+                }
+            });
 
 
+            console.log("Nb étoiles:" + newMagnitudes.length);
+            console.log("RA Range:", minRA, "-", maxRA);
+            console.log("DEC Range:", minDEC, "-", maxDEC);
 
-                const starsData = {
-                    vertices: coords,
-                    magnitudes: newMagnitudes,
-                    hipToIndex: hipToIndex
+            // Vérifier la gamme de magnitudes
+            const validMagnitudes = newMagnitudes.filter(mag => !isNaN(mag));
+
+            const minMagnitude = Math.min(...validMagnitudes);
+            const maxMagnitude = Math.max(...validMagnitudes);
+            console.log("Magnitude Range:", minMagnitude, "-", maxMagnitude);
+            const starsData = {
+                vertices: coords,
+                magnitudes: newMagnitudes,
+                hipToIndex: hipToIndex
+            };
+            // Stockez les données dans le contexte
+            setStarsData(starsData);
+            // Traitement de constellationLinesText
+            const constellationLines = constellationLinesText.split('\n').filter(line => !line.startsWith('#') && line.trim() !== '');
+            const parsedLines = constellationLines.map(line => {
+                const parts = line.split(',');
+                return {
+                    abbreviation: parts[0],
+                    group: parts[1],
+                    abbreviationGroup: parts[2],
+                    startStar: parseInt(parts[3]),
+                    endStar: parseInt(parts[4])
                 };
-                // Stockez les données dans le contexte
-                setStarsData(starsData);
             });
 
-            fetch(`${process.env.PUBLIC_URL}/datas/constellation_line_hip.csv`)
-           
-            .then(response => response.text())
-            .then(data => {
-                const lines = data.split('\n').filter(line => !line.startsWith('#') && line.trim() !== '');
-                const parsedLines = lines.map(line => {
-                    // console.log("Line=", line)
-                    const parts = line.split(',');
-                    return {
-                        abbreviation: parts[0],
-                        group: parts[1],
-                        abbreviationGroup: parts[2],
-                        startStar: parseInt(parts[3]),
-                        endStar: parseInt(parts[4])
-                    };
-                });
-
-                setConstellationLines(parsedLines);
-            });
+            setConstellationLines(parsedLines);
+            setIsLoaded(true);  // Mettre à jour isLoaded à true une fois que tout est chargé
+        });
     }, []);
 
-
     return (
-        <SkyContext.Provider value={{ shownConstellations,toggleShownConstellations,maxShownMagnitude, setMaxShownMagnitude, starsData, setStarsData, representation, setRepresentation, currentTime, location, toggleRepresentation, constellationLines }}>
+        <SkyContext.Provider value={{ isLoaded,shownConstellations, toggleShownConstellations, maxShownMagnitude, setMaxShownMagnitude, starsData, setStarsData, representation, setRepresentation, currentTime, location, toggleRepresentation, constellationLines, isLoaded }}>
             {children}
         </SkyContext.Provider>
     );
